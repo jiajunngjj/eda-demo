26901dc38a2eef960ec73fa0e1f478801285bfbf
wohshon <wohshon@gmail.com>
2020-06-04 13:49:57 +0800

    proxy SSE

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -8,6 +8,42 @@ router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Express', host: process.env.URL });
 });
 
+router.get('/stream', function(req,res,next){
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.flushHeaders(); // flush the headers to establish SSE with client
+
+    let interValID = setInterval(() => {
+        const options = {
+            host: 'order-eda-demo.apps.cluster-sgp-ca65.sgp-ca65.example.opentlc.com',
+            port: 80,
+            path: '/rest/orders/status',
+            method: 'GET',
+            headers: {
+              'Accept': 'text/event-stream'
+            }
+          };
+
+          var httpreq = http.get(options, function(httpres) {
+            //console.log('STATUS: ' + res.statusCode);
+            //console.log('HEADERS: ' + JSON.stringify(res.headers));
+          
+            // Buffer the body entirely for processing as a whole.
+            //var bodyChunks = [];
+            httpres.on('data', function(chunk) {
+              // You can process streamed parts here...
+              //bodyChunks.push(chunk);
+              res.write(chunk);
+            }).on('end', function() {
+              //var body = Buffer.concat(bodyChunks);
+              console.log('end');
+            })
+          });
+
+        //res.write(`data: ${JSON.stringify({num: counter})}\n\n`); // res.write() instead of res.send()
+    }, 1000);    
+} );
 router.post('/submit', function(req, res, next) {
   console.log(req.body);
   console.log(req.body.orderId);
